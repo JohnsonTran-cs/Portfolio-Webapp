8770e676b93d6b9bbd81aaaa9a2194befa183849
JohnsonTran-cs <johnsontran333@gmail.com>
2025-12-06 00:01:59 -0800

    fixed relative names

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import Profile from "./components/Profile";
 import About from "./components/About";
 import Projects from "./components/Projects";
