3b45bba6bc8d83197eb605af6f59ccfd647974e2
JohnsonTran-cs <johnsontran333@gmail.com>
2025-12-06 00:24:38 -0800

    fixed character limit for about me page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,13 +3,11 @@ export default function About() {
     <div className="section">
       <h2 className="section-title">About Me</h2>
 
-      <p>
+      <p className="section-desc">
         I'm a computer science student passionate about learning and teaching core concepts.
         I love working in teams to further my skills in delegation, team work, and quick learning for new concepts.
-        <br></br>
         I've had the opportunity to work in teams and have gain experience as a team member and a team leader.
         During my time as a student, I've been able to take on these leadership roles and delegate tasks to suit each member's strengths.
-        <br></br>
         Within short time frames, I've been able to learn new libraries and frameworks in order to produce acceptable products. 
         I love being able to work in new environments since it gives me the opportunity to learn and broaden my horizons.
       </p>
